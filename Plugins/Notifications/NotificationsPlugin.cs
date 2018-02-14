1ae4048d0a92054f5300829d013b6618938dc8e0
Romain Aubert <romain.aubert@swisstiming.com>
2018-02-14 16:31:23 +0100

    New plugin Notifications

---
+++ Plugins/Notifications/NotificationsPlugin.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+using GitCommands;
+using GitUIPluginInterfaces;
+using ResourceManager;
+
+namespace Notifications
+{
+    public class NotificationsPlugin : GitPluginBase, IGitPluginForRepository
+    {
+        public NotificationsPlugin()
+        {
+            SetNameAndDescription("Windows notifications");
+            Translate();
+        }
+
+        private IDisposable cancellationToken;
+        private IGitUICommands currentGitUiCommands;
+        private Notifier _Notifier;
+
+        private NumberSetting<int> CheckUpdateInterval = new NumberSetting<int>("Check update every (seconds) - set to 0 to disable", 0);
+        private BoolSetting ShowNotification = new BoolSetting("Show window desktop notification", false);
+        private StringSetting Repositories = new StringSetting("Repositories to check separated by ;", "");
+
+        public override IEnumerable<ISetting> GetSettings()
+        {
+            //return all settings or introduce implementation based on reflection on GitPluginBase level
+            yield return CheckUpdateInterval;
+            yield return ShowNotification;
+            yield return Repositories;
+        }
+
+        public override void Register(IGitUICommands gitUiCommands)
+        {
+            base.Register(gitUiCommands);
+
+            currentGitUiCommands = gitUiCommands;
+            currentGitUiCommands.PostSettings += OnPostSettings;
+
+            _Notifier = new Notifier(currentGitUiCommands);
+
+            RecreateObservable();
+        }
+
+        private void OnPostSettings(object sender, GitUIPostActionEventArgs e)
+        {
+            RecreateObservable();
+        }
+
+        private void RecreateObservable()
+        {
+            CancelBackgroundOperation();
+
+            int fetchInterval = CheckUpdateInterval.ValueOrDefault(Settings);
+
+            var gitModule = currentGitUiCommands.GitModule;
+            if (fetchInterval > 0 && gitModule.IsValidGitWorkingDir())
+            {
+                cancellationToken =
+                    Observable.Timer(TimeSpan.FromSeconds(Math.Max(5, fetchInterval)))
+                              .SelectMany(i =>
+                              {
+                                  // if git not runing - start fetch immediately
+                                  if (!gitModule.IsRunningGitProcess())
+                                      return Observable.Return(i);
+
+                                  // in other case - every 5 seconds check if git still runnnig
+                                  return Observable
+                                      .Interval(TimeSpan.FromSeconds(5))
+                                      .SkipWhile(ii => gitModule.IsRunningGitProcess())
+                                      .FirstAsync()
+                                  ;
+                              })
+                              .Repeat()
+                              .ObserveOn(ThreadPoolScheduler.Instance)
+                              .Subscribe(i =>
+                              {
+                                  var gitCmd = "fetch --all";
+
+                                  // Loop on each repository
+                                  string repos = Repositories.ValueOrDefault(Settings);
+                                  foreach (string repo in repos.Split(';'))
+                                  {
+                                      // Create git module to fetch this repo
+                                      GitModule module = new GitModule(repo);
+                                      string res = module.RunGitCmd(gitCmd);
+
+                                      // Conditions informing repo has changed
+                                      if (res.Contains("From"))
+                                      {
+                                          if (ShowNotification.ValueOrDefault(Settings))
+                                          {
+                                              _Notifier.RepoUpdated(repo);
+                                          }
+
+                                          // Update UI if it's current repo
+                                          if (gitModule.WorkingDir == module.WorkingDir)
+                                          {
+                                              currentGitUiCommands.RepoChangedNotifier.Notify();
+                                          }
+                                      }
+                                  }
+                              }
+                                  );
+            }
+        }
+
+        private void CancelBackgroundOperation()
+        {
+            if (cancellationToken != null)
+            {
+                cancellationToken.Dispose();
+                cancellationToken = null;
+            }
+        }
+
+        public override void Unregister(IGitUICommands gitUiCommands)
+        {
+            CancelBackgroundOperation();
+
+            if (currentGitUiCommands != null)
+            {
+                currentGitUiCommands.PostSettings -= OnPostSettings;
+                currentGitUiCommands = null;
+            }
+
+            if (_Notifier != null)
+            {
+                _Notifier.Dispose();
+            }
+
+            base.Unregister(gitUiCommands);
+        }
+
+        public override bool Execute(GitUIBaseEventArgs gitUiArgs)
+        {
+            gitUiArgs.GitUICommands.StartSettingsDialog(this);
+            return false;
+        }
+    }
+}
---
+++ Plugins/Notifications/Notifier.cs
@@ -0,0 +1,118 @@
+﻿using GitCommands;
+using GitUI;
+using GitUIPluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notifications
+{
+    public class Notifier : IDisposable
+    {
+        private NotifyIcon _NotifyIcon = null;
+        private IGitUICommands _GitUiCommands = null;
+
+        private List<string> _reposUpdated = new List<string>();
+
+        public Notifier(IGitUICommands gitUICommands)
+        {
+            _GitUiCommands = gitUICommands;
+
+            _NotifyIcon = new NotifyIcon();
+            _NotifyIcon.BalloonTipClicked += _notifyIcon_BalloonTipClicked;
+            _NotifyIcon.Visible = true;
+
+            UpdateIcon();
+            UpdateMenu();
+        }
+
+        private void OnMenu(object sender, EventArgs e)
+        {
+            if (sender is MenuItem mi && mi.Tag != null && mi.Tag.ToString() == "Clear")
+            {
+                _reposUpdated.Clear();
+
+                UpdateMenu();
+                UpdateIcon();
+            }
+        }
+
+        private void _notifyIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+        }
+        
+        public void RepoUpdated(string repoPath)
+        {
+            if (!_reposUpdated.Contains(repoPath))
+            {
+                _reposUpdated.Add(repoPath);
+
+                // Shows a notification with specified message and title
+                _NotifyIcon.ShowBalloonTip(3000, "Repository updated", $"Repo {repoPath} has changed !", ToolTipIcon.Info);
+            }
+
+            UpdateIcon();
+            UpdateMenu();
+        }
+
+        private void UpdateIcon()
+        {
+            if (_reposUpdated.Count > 0)
+            {
+                _NotifyIcon.Icon = Notifications.Properties.Resources.git_extensions_logo_final_red;
+            }
+            else
+            {
+                _NotifyIcon.Icon = Notifications.Properties.Resources.git_extensions_logo_final_green;
+            }
+        }
+
+        private void UpdateMenu()
+        {
+            if (_reposUpdated.Count > 0)
+            {
+                List<MenuItem> menus = new List<MenuItem>();
+                foreach (var repo in _reposUpdated)
+                {
+                    var menu = new MenuItem($"Repo {repo} has changed !", OnMenu)
+                    {
+                        Tag = repo
+                    };
+                    menus.Add(menu);
+                }
+
+                var menuClear = new MenuItem($"Clear", OnMenu)
+                {
+                    Tag = "Clear"
+                };
+                menus.Add(menuClear);
+
+
+                _NotifyIcon.ContextMenu = new ContextMenu(menus.ToArray());
+            }
+            else
+            {
+                MenuItem[] menus = { new MenuItem("No change") };
+                _NotifyIcon.ContextMenu = new ContextMenu(menus);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_NotifyIcon != null)
+            {
+                _NotifyIcon.Visible = false;
+                _NotifyIcon.BalloonTipClicked -= _notifyIcon_BalloonTipClicked;
+                _NotifyIcon.Dispose();
+                _NotifyIcon = null;
+            }
+
+            _GitUiCommands = null;
+        }
+    }
+}
